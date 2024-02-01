1c7865fe8d383acf142824fd43ac6d8a8ea9f08b
Igor Sales <igor.guitarman@gmail.coms>
2024-01-31 21:22:39 -0300

    Primeiro commit

--- App.js
+++ App.js
@@ -1,10 +1,27 @@
+import { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, TouchableOpacity, Modal} from 'react-native';
+import {ModalPlayer} from './src/components/modal'
+
+const colors = ['#d33833', '#ea6a3f', '#706f6b', '#32312f'];
 
 export default function App() {
+  const [modalVisible, setModalVisible] = useState(false);
+
+  function click() {
+    console.log("Clicado e aprovado, chefe!")
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Image source={require("./src/assets/logo.png")} style={styles.logo}/>
+      <Text style={styles.title}>Minder</Text>
+      <TouchableOpacity style={styles.button} onPress={click}>
+        <Text style={styles.buttonText}>Jogar</Text>
+      </TouchableOpacity>
+      <Modal visible={modalVisible} animationType='fade'>
+        <ModalPlayer/>
+      </Modal>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +30,31 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#f3f3ff',
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+  logo: {
+    marginBottom: -25,
+    width: 300,
+    height: 320
+  },
+  title: {
+    marginBottom: 60,
+    fontSize: 35,
+    fontWeight: 'bold'
+  },
+  button: {
+    backgroundColor: colors[0],
+    width: "30%",
+    height: 50,
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: 10
+  },
+  buttonText: {
+    color: "#fff",
+    fontSize: 20,
+    fontWeight: 'bold'
+  },
+}); 
---
+++ src/components/modal/index.js
@@ -0,0 +1,15 @@
+import { StyleSheet, Text, View, Image, TouchableOpacity} from 'react-native';
+
+export function ModalPlayer(){
+    return(
+        <View>
+            <Text>Teste Modal</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container:{
+
+    }
+})
